1b9a833412fa8cc9aa0cc1dffc7bcff367ad8139
phriscage <phriscage@gmail.com>
2019-04-16 17:09:08 -0400

    set a notify template and custom ccss

--- cloud/controller-app/src/store.js
+++ cloud/controller-app/src/store.js
@@ -4,6 +4,16 @@ import client from 'api-client';
 
 Vue.use(Vuex);
 
+function notify(type, response) {
+  Vue.notify({
+    group: 'main',
+    title: `${response.status}: ${response.statusText}`,
+    text: response.data,
+    type: type,
+    data: response
+  });
+}
+
 const store = new Vuex.Store({
   state: {
     config: {},
@@ -49,40 +59,65 @@ const store = new Vuex.Store({
   actions: {
     // Config
     getConfig({ commit }) {
-      return client.getConfig().then(config => commit('setConfig', config));
+      return client
+        .getConfig()
+        .then(config => commit('setConfig', config))
+        .catch(error => {
+          notify('warn', error.response);
+        });
     },
     updateConfig({ commit }, payload) {
       return client
         .updateConfig(payload)
-        .then(config => commit('setConfig', config));
+        .then(config => commit('setConfig', config))
+        .catch(error => {
+          notify('warn', error.response);
+        });
     },
     // ConfigOptions
     getConfigOptions({ commit }) {
       return client
         .getConfigOptions()
-        .then(configOptions => commit('setConfigOptions', configOptions));
+        .then(configOptions => commit('setConfigOptions', configOptions))
+        .catch(error => {
+          notify('warn', error.response);
+        });
     },
     // Stats
     getStats({ commit }) {
-      return client.getStats().then(stats => commit('setStats', stats));
+      return client
+        .getStats()
+        .then(stats => commit('setStats', stats))
+        .catch(error => {
+          notify('warn', error.response);
+        });
     },
     // Messages
     getMessages({ commit }) {
       return client
         .getMessages()
-        .then(messages => commit('setMessages', messages));
+        .then(messages => commit('setMessages', messages))
+        .catch(error => {
+          notify('warn', error.response);
+        });
     },
     // DebugMessages
     createDebugMessage({ commit }) {
       return client
         .createDebugMessage()
-        .then(debugMessage => commit('setDebugMessage', debugMessage));
+        .then(debugMessage => commit('setDebugMessage', debugMessage))
+        .catch(error => {
+          notify('warn', error.response);
+        });
     },
     // DrivingMessages
     createDrivingMessage({ commit }) {
       return client
         .createDrivingMessage()
-        .then(drivingMessage => commit('setDrivingMessage', drivingMessage));
+        .then(drivingMessage => commit('setDrivingMessage', drivingMessage))
+        .catch(error => {
+          notify('warn', error.response);
+        });
     },
     // Settings
     getSettings({ commit }) {
@@ -92,14 +127,20 @@ const store = new Vuex.Store({
       if (!this._vm.$session.get('settings')) {
         return client
           .getSettings()
-          .then(settings => commit('setSettings', settings));
+          .then(settings => commit('setSettings', settings))
+          .catch(error => {
+            notify('warn', error.response);
+          });
       }
       return commit('setSettings', this._vm.$session.get('settings'));
     },
     updateSettings({ commit }, payload) {
       return client
         .updateSettings(payload)
-        .then(settings => commit('setSettings', settings));
+        .then(settings => commit('setSettings', settings))
+        .catch(error => {
+          notify('warn', error.response);
+        });
     },
     resetSettings({ commit }) {
       if (!this._vm.$session.exists()) {
@@ -108,7 +149,10 @@ const store = new Vuex.Store({
       this._vm.$session.clear();
       return client
         .getSettings()
-        .then(settings => commit('setSettings', settings));
+        .then(settings => commit('setSettings', settings))
+        .catch(error => {
+          notify('warn', error.response);
+        });
     }
   }
 });
