5462203d1c9b23fdbbb7993301f6d8afbf9e2dfc
phriscage <phriscage@gmail.com>
2019-06-11 19:57:20 -0400

    updated Messages List view with tooltip

--- cloud/controller-app/src/components/MessagesList.vue
+++ cloud/controller-app/src/components/MessagesList.vue
@@ -20,66 +20,50 @@
         </v-img>
 
         <v-card-text>
-          <v-list one-line subheader>
-            <v-subheader>inboundMsgHistory</v-subheader>
-            <v-divider></v-divider>
-            <template v-for="(message, index) in messages.inboundMsgHistory">
+          <v-list>
+            <v-list-group
+              v-for="(key, index) in Object.keys(messages)"
+              :key="index"
+              prepend-icon="list"
+              no-action
+            >
+              <template v-slot:activator>
+                <v-list-tile>
+                  <v-list-tile-content>
+                    <v-list-tile-title>{{ key }}</v-list-tile-title>
+                  </v-list-tile-content>
+                </v-list-tile>
+                {{ messages[key].length }}
+              </template>
+
               <v-list-tile
-                :key="message.id"
+                v-for="(message, messageIndex) in messages[key]"
+                :key="messageIndex"
                 avatar
-                @click="toggle(message, index)"
               >
                 <v-list-tile-content>
-                  <v-list-tile-title>
-                    {{ message.id }}
-                  </v-list-tile-title>
                   <v-list-tile-sub-title>
-                    {{ message.name }}
+                    {{ message }}
                   </v-list-tile-sub-title>
                 </v-list-tile-content>
 
                 <v-list-tile-action>
-                  <v-btn icon ripple>
-                    <v-icon color="grey lighten-1">info</v-icon>
-                  </v-btn>
+                  <v-tooltip close-delay="1500" left>
+                    <template v-slot:activator="{ on }">
+                      <v-btn
+                        v-on="on"
+                        icon
+                        @click="toggle(message, messageIndex)"
+                        ripple
+                      >
+                        <v-icon color="grey lighten-1">info</v-icon>
+                      </v-btn>
+                    </template>
+                    <span>{{ message }}</span>
+                  </v-tooltip>
                 </v-list-tile-action>
               </v-list-tile>
-              <v-divider
-                v-if="index + 1 < messages.inboundMsgHistory.length"
-                :key="index"
-              ></v-divider>
-            </template>
-          </v-list>
-
-          <v-list one-line subheader>
-            <v-subheader>outboundMsgHistory</v-subheader>
-            <v-divider></v-divider>
-            <template v-for="(message, index) in messages.outboundMsgHistory">
-              <v-list-tile
-                :key="message.id"
-                avatar
-                @click="toggle(message, index)"
-              >
-                <v-list-tile-content>
-                  <v-list-tile-title>
-                    {{ message.id }}
-                  </v-list-tile-title>
-                  <v-list-tile-sub-title>
-                    {{ message.name }}
-                  </v-list-tile-sub-title>
-                </v-list-tile-content>
-
-                <v-list-tile-action>
-                  <v-btn icon ripple>
-                    <v-icon color="grey lighten-1">info</v-icon>
-                  </v-btn>
-                </v-list-tile-action>
-              </v-list-tile>
-              <v-divider
-                v-if="index + 1 < messages.outboundMsgHistory.length"
-                :key="index"
-              ></v-divider>
-            </template>
+            </v-list-group>
           </v-list>
           <v-btn @click="refresh">refresh</v-btn>
         </v-card-text>
@@ -117,7 +101,7 @@ export default {
       this.loadMessages();
     },
     toggle(message, index) {
-      console.log(`toggle clicked: [${index}], ${message.id}`);
+      console.log(`toggle clicked: [${index}], `, message);
     }
   }
 };
